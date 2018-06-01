3c944e9cc292c3d5ecfe02def4acd88efbf387a9
Fab-one <info@fab-one.nl>
2018-06-01 23:26:07 +0200

    section 1 and 2 almost done

--- your_javascript_here.js
+++ your_javascript_here.js
@@ -3,8 +3,56 @@ var hero = {
   name: 'Frodo',
   heroic: true,
   inventory: [],
-  
-}
+  health: 10,
+  weapon: {type: '', damage: 0}
+};
+
+//var creature = {
+//  name: '',
+//  heroic: true,
+//  inventory: [],
+//  health: 10
+//  item: {type: '', damage: 0}
+//}
+
 // Game logic
+function rest(creature) {
+  creature.health = 10
+  return creature
+  }
+
+function pickUpItem(creature, item) {
+  creature.inventory.push(item)
+  return creature
+};
+
+function dealDamage(attacker, defender) {
+  defender.health = defender.health - attacker.weapon.damage
+  return defender
+};
+
+function equipWeapon(creature, index) {
+  creature.weapon = creature.inventory[index]
+  creature.inventory.splice(index, 1)
+  return creature
+};
+/* Take it from here, function not OK
+
+function doBattle(heroicCreature, creature) {
+  if (heroicCreature.heroic(false)) {
+    return null
+  }
+}
+  while (heroicCreature.health > 0 && creature.health > 0) {
+    dealDamage(heroicCreature, creature)
+    dealDamage(creature, heroicCreature)
+    if (creature.health <= 0 && heroicCreature.health > 0) {
+      return heroicCreature.health
+    } else {
+      window.alert('Your hero is dead!')
+    }
+  }
+
+*/
 
 // UI
