5d8e7ecda5bb1fe442bcb8d724433e6ae7567deb
Fab-one <info@fab-one.nl>
2018-06-03 09:19:16 +0200

    section 1 done, section 2 and 3 almost done

--- your_javascript_here.js
+++ your_javascript_here.js
@@ -2,18 +2,17 @@
 var hero = {
   name: 'Frodo',
   heroic: true,
-  inventory: [],
-  health: 10,
-  weapon: {type: '', damage: 0}
+  inventory: [{type: "gun", damage: 3}, {type: "bomb", damage: 10}, {type: "frying pan", damage: 1}],
+  health: 6,
+  weapon: {type: "knife", damage: 2}
 };
 
-//var creature = {
-//  name: '',
-//  heroic: true,
-//  inventory: [],
-//  health: 10
-//  item: {type: '', damage: 0}
-//}
+var enemy = {
+  name: 'Trump',
+  health: 2,
+  weapon: {type: "covfefe", damage: 1}
+};
+
 
 // Game logic
 function rest(creature) {
@@ -36,23 +35,59 @@ function equipWeapon(creature, index) {
   creature.inventory.splice(index, 1)
   return creature
 };
-/* Take it from here, function not OK
 
 function doBattle(heroicCreature, creature) {
-  if (heroicCreature.heroic(false)) {
+  if (!heroicCreature.heroic) {
     return null
   }
-}
-  while (heroicCreature.health > 0 && creature.health > 0) {
+
+  while (heroicCreature.health && creature.health > 0) {
     dealDamage(heroicCreature, creature)
     dealDamage(creature, heroicCreature)
     if (creature.health <= 0 && heroicCreature.health > 0) {
-      return heroicCreature.health
+      break;
+      return heroicCreature
     } else {
       window.alert('Your hero is dead!')
     }
+    }
   }
 
-*/
-
 // UI
+function clickBed(){
+  document.getElementById("bed")
+  rest(hero)
+  console.log("The health of " + hero.name + " is restored to " + hero.health)
+};
+
+function addWeaponToInventory(){
+  document.getElementById("weapon")
+  pickUpItem(hero,{type: "assaultRifle",damage: 2})
+  console.log(hero.inventory)
+};
+
+function battleWithEnemy(){
+  document.getElementById("enemy")
+  doBattle(hero,enemy)
+  console.log(hero.health)
+};
+
+function useBackPack(){
+  equipWeapon(hero,window.prompt("Enter indexnumber"))
+  console.log(hero.weapon.type)
+};
+
+function displayStats(){
+document.getElementById("display").innerHTML="Hero name " + hero.name + ". His health is " + hero.health + ". He carries a " + hero.weapon.type + " which inflicts " + hero.weapon.damage + " points damage.";
+};
+
+
+function updateStats(){
+  displayStats()
+
+}
+
+
+
+
+displayStats();
